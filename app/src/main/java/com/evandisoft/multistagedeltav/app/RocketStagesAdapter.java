ae59b95884cf28b136810c186d33bdb5707ef83c
developer <developer@maindebiancomp.myhome>
2018-11-03 00:08:36 -0700

    before adding "getGroupView" on RocketSTagesSadapter

--- app/src/main/java/com/evandisoft/multistagedeltav/app/RocketStagesAdapter.java
+++ app/src/main/java/com/evandisoft/multistagedeltav/app/RocketStagesAdapter.java
@@ -1,42 +1,55 @@
 package com.evandisoft.multistagedeltav.app;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 
+import java.text.DecimalFormat;
+
 class RocketStagesAdapter extends BaseExpandableListAdapter {
+    private Context context;
+    private DecimalFormat decimalFormat = new DecimalFormat();
+    int i = 0;
+    private LayoutInflater layoutInflater;
+    public Rocket rocket;
 
     public RocketStagesAdapter(MainActivity mainActivity, Rocket rocket) {
+        this.context = mainActivity;
+        this.layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.rocket = rocket;
+        rocket.rsa = this;
     }
 
     @Override
     public int getGroupCount() {
-        return 0;
+        return this.rocket.size();
     }
 
     @Override
     public int getChildrenCount(int groupPosition) {
-        return 0;
+        return 1;
     }
 
     @Override
     public Object getGroup(int groupPosition) {
-        return null;
+        return this.rocket.get(groupPosition);
     }
 
     @Override
     public Object getChild(int groupPosition, int childPosition) {
-        return null;
+        return this.rocket.get(groupPosition);
     }
 
     @Override
     public long getGroupId(int groupPosition) {
-        return 0;
+        return (long) groupPosition;
     }
 
     @Override
     public long getChildId(int groupPosition, int childPosition) {
-        return 0;
+        return (long) groupPosition;
     }
 
     @Override
